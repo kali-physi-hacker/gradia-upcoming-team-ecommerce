8a4dbe2a8917282081f00f3419a71576f3a01641
Desmond Brown <browndesmond30@yahoo.com>
2021-03-08 21:49:18 +0000

    Corrected title Product model field

--- products/models.py
+++ products/models.py
@@ -12,7 +12,7 @@ class DeletedManager(models.Manager):
 
 
 class Product(models.Model):
-    titles = models.CharField(max_length=100)
+    title = models.CharField(max_length=100)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     available = models.BooleanField(default=True)
