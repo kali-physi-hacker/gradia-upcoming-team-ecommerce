c08c2301e3e46e82cae94f397710c54d82c35680
RJ-0605 <64560428+RJ-0605@users.noreply.github.com>
2021-03-10 05:41:15 +0000

    implemented api_views for get of single product for review

--- products/views.py
+++ products/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+
+
 
 
 def test_view(request):
@@ -9,3 +14,14 @@ def test_view(request):
     template = "index.html"
     context = {}
     return render(request=request, template_name=template, context=context)
+
+
+
+# gets a single product 
+@api_view(['GET'])
+def get_singleproduct() :
+    """
+        this to show we can get a single product 
+
+    """
+    return Response({"message": "Hello for today! See you tomorrow!"}) 
