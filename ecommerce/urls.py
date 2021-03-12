0b8242e61479d3aed5aceed26c07e261c0f1390b
RJ-0605 <64560428+RJ-0605@users.noreply.github.com>
2021-03-12 22:02:59 +0000

    implemented changes for the product detail for PUT GET and DELETE

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from products.views import test_view
 
-urlpatterns = [path("admin/", admin.site.urls), path("home/", test_view)]
+urlpatterns = [
+    path("admin/", admin.site.urls), 
+    path("home/", test_view), 
+    path("api/", include("products.urls")),
+    ]
--- products/views.py
+++ products/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 
-
-
+from products.models import Product
+from products.serializers import ProductSerializer
 
 
 def test_view(request):
@@ -18,10 +19,38 @@ def test_view(request):
 
 
 # gets a single product 
-@api_view(['GET'])
-def get_singleproduct() :
+@api_view(['GET', 'DELETE','PUT'])
+def product_detail(request, pk):
     """
-        this to show we can get a single product 
+    Gets a single product
+    :param request:
+    :param pk:
+    returns:
+    """
+    try:
+        product = Product.objects.get(pk=pk)
+    except Product.DoesNotExist:
+        return Response(data={"error": "Product does not exist"}, status=status.HTTP_404_NOT_FOUND)
 
-    """
-    return Response({"message": "Hello for today! See you tomorrow!"}) 
+    if request.method == 'GET':
+        serializer = ProductSerializer(product)
+        return Response(data={"product": serializer.data}, status=status.HTTP_200_OK) 
+    
+    elif request.method == "PUT":
+        serializer = ProductSerializer(instance=product, data=request.data)
+        if not serializer.is_valid():
+            return Response(data={"errors": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+        serializer.save()
+
+        return Response(data={"product":serializer.data}, status=status.HTTP_201_CREATED)
+    
+    elif request.method == 'DELETE':
+        serializer = ProductSerializer(instance=product, data={"is_active":False})
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(data={"message": "Product deleted successfully"}, status=status.HTTP_204_NO_CONTENT)
+
+            
+    
+   
